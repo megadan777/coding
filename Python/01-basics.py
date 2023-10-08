081e0e719dd74b5a555125516938a8ecc0e680e1
megadan777 <daniel.mowbray86@gmail.com>
2023-10-08 20:06:22 +1100

    Python

---
+++ Python/01-basics.py
@@ -0,0 +1,21 @@
+#Variables
+one = 1
+
+two = 2
+some_number = 1000
+
+# booleans
+true_boolean = True
+false_boolean = False
+
+# string
+my_name = "Leandro Tk"
+
+# float
+book_price = 15.80
+
+if True:
+  print("Hello Python")
+
+if 2 > 1:
+  print("2 is greater than 1")
