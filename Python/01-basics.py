e6c6620a9b029d74d32e5469877c7e279881f806
megadan777 <daniel.mowbray86@gmail.com>
2023-10-08 20:19:52 +1100

    Python

--- Python/01-basics.py
+++ Python/01-basics.py
@@ -19,3 +19,10 @@ if True:
 
 if 2 > 1:
   print("2 is greater than 1")
+
+  if 1 > 2:
+    print("1 is greater than 2")
+elif 2 > 1:
+  print("1 is not greater than 2")
+else:
+  print("1 is equal to 2")
